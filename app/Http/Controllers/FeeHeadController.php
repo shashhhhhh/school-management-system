4208a9ae7ee28d336aeed1ab82072c163185d50e
Janitha Madushan <madhushanamg@gmail.com>
2025-03-03 11:29:58 +0530

    Class management and Fee management

---
+++ app/Http/Controllers/FeeHeadController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\FeeHead;
+use Illuminate\Http\Request;
+
+class FeeHeadController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('admin.fee-head.fee-head');
+    }
+
+ 
+    public function store(Request $request)
+    {
+        $request->validate([
+                'name'=>'required'
+            ]);
+            $fee = new FeeHead();
+            $fee->name = $request->name;
+            $fee->save();
+            return redirect()->route('fee-head.create')->with('success','Fee Head Added Successfully');
+
+    }
+
+
+    public function read()
+    {
+        $data ['fee']= FeeHead::latest()->get();
+        return view('admin.fee-head.fee-head-list', $data);
+    }
+
+    public function edit($id)
+    {
+        $data ['fee'] = FeeHead::find($id);
+        return view('admin.fee-head.edit-fee-head', $data);
+    }
+
+    public function update(Request $request)
+    {
+        $fee = FeeHead::find($request->id);
+        $fee->name = $request->name;
+        $fee->update();
+        return redirect()->route('fee-head.read')->with('success','Fee Head Updated Successfully');
+    }
+
+    public function delete($id)
+    {
+        $fee = FeeHead::find($id);
+        $fee->delete();
+        return redirect()->back()->with('success','Fee Head Deleted Successfully');
+    }
+
+}
--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -208,7 +208,7 @@
 
                         
                         <li class="nav-item">
-                            <a href="pages/widgets.html" class="nav-link">
+                            <a href="{{route('admin.dashboard')}}" class="nav-link">
                                 <i class="nav-icon fas fa-th"></i>
                                 <p>
                                     Dashboard
@@ -233,6 +233,78 @@
                                 </li>
                                 <li class="nav-item">
                                     <a href="{{route('academic-year.read')}}" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>View Record</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-chart-pie"></i>
+                                <p>
+                                    Class Management
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{route('class.create')}}" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Add Record</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{route('class.read')}}" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>View Record</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-chart-pie"></i>
+                                <p>
+                                    Fee Head Management
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{route('fee-head.create')}}" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Add Record</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{route('fee-head.read')}}" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>View Record</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-chart-pie"></i>
+                                <p>
+                                    Fee Structure Management
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="{{route('fee-structure.create')}}" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Add Record</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="{{route('fee-structure.read')}}" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>View Record</p>
                                     </a>
