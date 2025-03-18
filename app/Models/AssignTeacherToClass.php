d77bd9fe86a05fe83425d714a9963ef68a4b96e2
Abishek-WebDeveloper <mohanabishek2705gmail.com>
2025-03-18 14:56:41 +0530

    Updated AssignTeacherToClass model with necessary modifications

--- app/Models/AssignTeacherToClass.php
+++ app/Models/AssignTeacherToClass.php
@@ -10,23 +10,24 @@ use App\Models\Subject;
 class AssignTeacherToClass extends Model
 {
     use HasFactory;
+
     protected $fillable = [
-        'class_id','subject_id','teacher_id'
+        'class_id', 'subject_id', 'teacher_id',
     ];
-    cla
+
     public function class()
     {
-      return $this->belongsTo(Classes::class,'class_id');
+        return $this->belongsTo(Classes::class, 'class_id');
     }
 
     public function subject()
     {
-      return $this->belongsTo(Subject::class,'subject_id');
+        return $this->belongsTo(Subject::class, 'subject_id');
     }
 
     public function teacher()
     {
-      return $this->belongsTo(User::class,'teacher_id');
+        return $this->belongsTo(User::class, 'teacher_id');
     }
 }
 
